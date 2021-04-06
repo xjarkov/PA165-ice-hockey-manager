162c32ff60d2aabb54933aa9be019cbd1593ef94
Lukas Machalek <xmachal6@fi.muni.cz>
2021-04-06 18:12:43 +0200

    Match first implementation

--- src/main/java/cz/fi/muni/pa165/hockeymanager/entity/Match.java
+++ src/main/java/cz/fi/muni/pa165/hockeymanager/entity/Match.java
@@ -1,4 +1,89 @@
 package cz.fi.muni.pa165.hockeymanager.entity;
 
+import java.sql.Time;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
 public class Match {
+
+    @Id
+    @GeneratedValue
+    @Getter
+    private long id;
+
+    @ManyToOne
+    @Getter
+    private Team homeTeam;
+
+    @ManyToOne
+    @Getter
+    private Team visitingTeam;
+
+    @NotNull
+    @Getter
+    @Setter
+    private int homeTeamScore;
+
+    @NotNull
+    @Getter
+    @Setter
+    private int visitingTeamScore;
+
+    @NotNull
+    @Getter
+    private Date date;
+
+    @NotNull
+    @Getter
+    private Time time;
+
+    public Match(){}
+
+    public Match(Team homeTeam, Team visitingTeam, Date date, Time time) {
+        this.homeTeam = homeTeam;
+        this.visitingTeam = visitingTeam;
+        this.date = date;
+        this.time = time;
+
+        homeTeamScore = 0;
+        visitingTeamScore = 0;
+    }
+
+    public void simulateMatch(){
+        int max = 5;
+        int min = 0;
+        visitingTeamScore = (int) (Math.random() * (max - min)) + min;
+        homeTeamScore = (int) (Math.random() * (max - min)) + min;
+
+    }
+
+    public void addHomeScore(){
+        homeTeamScore++;
+    }
+
+    public void addVisitingScore(){
+        visitingTeamScore++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Match)) return false;
+        Match match = (Match) o;
+        return id == match.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, homeTeam, visitingTeam, homeTeamScore, visitingTeamScore, date, time);
+    }
 }
