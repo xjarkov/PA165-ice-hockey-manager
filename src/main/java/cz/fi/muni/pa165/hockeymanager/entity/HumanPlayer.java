fa79f8a0e8360c62312980d1b0b423239715fe34
Petr Sopf <trsak1@seznam.cz>
2021-04-06 18:09:45 +0200

    Added HumanPlayer entity

--- src/main/java/cz/fi/muni/pa165/hockeymanager/entity/HumanPlayer.java
+++ src/main/java/cz/fi/muni/pa165/hockeymanager/entity/HumanPlayer.java
@@ -1,4 +1,85 @@
 package cz.fi.muni.pa165.hockeymanager.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
 public class HumanPlayer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @Getter
+    @Setter
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    @Getter
+    @Setter
+    private String email;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private String password;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private boolean isAdmin = false;
+
+    @Column
+    @OneToOne
+    @Getter
+    @Setter
+    private Team team;
+
+    @Override
+    public String toString() {
+        return "Human player {" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email=" + email +
+                ", isAdmin=" + isAdmin +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof HumanPlayer))
+            return false;
+
+        HumanPlayer other = (HumanPlayer) obj;
+        if (name == null) {
+            if (other.getName() != null)
+                return false;
+        } else if (!name.equals(other.getName()))
+            return false;
+
+        if (email == null) {
+            if (other.getEmail() != null)
+                return false;
+        } else if (!email.equals(other.getEmail()))
+            return false;
+
+        return true;
+    }
 }
