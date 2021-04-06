c1f5291bad38eadd555ff86c5611c6b9349ddf6b
Petr Sopf <trsak1@seznam.cz>
2021-04-06 19:12:04 +0200

    Added PlayerDao interface

--- src/main/java/cz/fi/muni/pa165/hockeymanager/dao/PlayerDao.java
+++ src/main/java/cz/fi/muni/pa165/hockeymanager/dao/PlayerDao.java
@@ -1,4 +1,29 @@
 package cz.fi.muni.pa165.hockeymanager.dao;
 
+import cz.fi.muni.pa165.hockeymanager.entity.Player;
+
+import java.util.List;
+
 public interface PlayerDao {
+    /**
+     * Creates new player
+     * @param player Player entity
+     */
+    void create(Player player);
+
+    /**
+     * Removes player by ID
+     * @param playerId Player ID
+     */
+    void remove(Long playerId);
+
+    /**
+     * @return List of all players
+     */
+    List<Player> findAll();
+
+    /**
+     * @return Player by it's id
+     */
+    Player findById(Long id);
 }
