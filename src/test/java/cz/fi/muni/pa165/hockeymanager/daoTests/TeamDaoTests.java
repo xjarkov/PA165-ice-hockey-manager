50f873293ab5ab6e47198834575b47fb150fbfcb
Lukas Machalek <xmachal6@fi.muni.cz>
2021-04-07 17:26:56 +0200

    Create team test

--- src/test/java/cz/fi/muni/pa165/hockeymanager/daoTests/TeamDaoTests.java
+++ src/test/java/cz/fi/muni/pa165/hockeymanager/daoTests/TeamDaoTests.java
@@ -1,4 +1,38 @@
 package cz.fi.muni.pa165.hockeymanager.daoTests;
 
-public class TeamDaoTests {
+import cz.fi.muni.pa165.hockeymanager.PersistenceApplicationContext;
+import cz.fi.muni.pa165.hockeymanager.dao.TeamDao;
+import cz.fi.muni.pa165.hockeymanager.entity.Match;
+import cz.fi.muni.pa165.hockeymanager.entity.Team;
+import cz.fi.muni.pa165.hockeymanager.enums.Championship;
+import java.util.Calendar;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ContextConfiguration(classes = PersistenceApplicationContext.class)
+@Transactional
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+public class TeamDaoTests extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private TeamDao teamDao;
+
+    @Test
+    public void createTeamTest() {
+        Team team = new Team("team", Championship.SHL);
+
+        teamDao.create(team);
+        assertThat(teamDao.findById(team.getId())).isEqualTo(team);
+    }
+
+
+
+
 }
