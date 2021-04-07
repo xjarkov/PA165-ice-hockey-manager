3d4195256ee55d76b1e71b25cf5e7ac1bd83dafc
xkosorin <kristian@kosorin.eu>
2021-04-07 19:17:47 +0200

    Unified annotations in entity classes

--- src/main/java/cz/fi/muni/pa165/hockeymanager/entity/Player.java
+++ src/main/java/cz/fi/muni/pa165/hockeymanager/entity/Player.java
@@ -3,10 +3,8 @@ package cz.fi.muni.pa165.hockeymanager.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.Objects;
 
 @Entity
 public class Player {
