22d382d68deeba9c8964762c2836aa1d34fe86bd
Lukas Machalek <xmachal6@fi.muni.cz>
2021-04-28 22:48:47 +0200

    Added simple match service interface and impl

--- manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/MatchService.java
+++ manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/MatchService.java
@@ -1,4 +1,14 @@
 package cz.fi.muni.pa165.hockeymanager.service;
 
+import cz.fi.muni.pa165.hockeymanager.entity.Match;
+import java.util.List;
+
 public interface MatchService {
+
+    void createMatch(Match match);
+    void removeMatch(Match match);
+
+    Match getById(Long matchId);
+
+    List<Match> getAllMatches();
 }
--- manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/MatchServiceImpl.java
+++ manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/MatchServiceImpl.java
@@ -1,4 +1,32 @@
 package cz.fi.muni.pa165.hockeymanager.service;
 
-public class MatchServiceImpl {
+import cz.fi.muni.pa165.hockeymanager.dao.MatchDao;
+import cz.fi.muni.pa165.hockeymanager.entity.Match;
+import java.util.List;
+import org.dozer.inject.Inject;
+
+public class MatchServiceImpl implements MatchService {
+
+    @Inject
+    private MatchDao matchDao;
+
+    @Override
+    public void createMatch(Match match) {
+        matchDao.create(match);
+    }
+
+    @Override
+    public void removeMatch(Match match) {
+        matchDao.remove(match);
+    }
+
+    @Override
+    public Match getById(Long matchId) {
+        return matchDao.findById(matchId);
+    }
+
+    @Override
+    public List<Match> getAllMatches() {
+        return matchDao.findAll();
+    }
 }
