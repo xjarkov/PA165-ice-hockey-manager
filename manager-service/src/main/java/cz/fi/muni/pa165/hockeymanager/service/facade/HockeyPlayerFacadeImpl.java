5397011c1d103402bd5664848878c668c30daf0f
Lukas Machalek <xmachal6@fi.muni.cz>
2021-04-30 01:01:19 +0200

    Implemented HockeyPlayerFacadeImpl, check the comments in PR thx

--- manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/facade/HockeyPlayerFacadeImpl.java
+++ manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/facade/HockeyPlayerFacadeImpl.java
@@ -1,33 +1,60 @@
 package cz.fi.muni.pa165.hockeymanager.service.facade;
 
 import cz.fi.muni.pa165.hockeymanager.dto.HockeyPlayerDto;
+import cz.fi.muni.pa165.hockeymanager.entity.HockeyPlayer;
+import cz.fi.muni.pa165.hockeymanager.entity.Team;
 import cz.fi.muni.pa165.hockeymanager.facade.HockeyPlayerFacade;
+import cz.fi.muni.pa165.hockeymanager.service.BeanMappingService;
 import cz.fi.muni.pa165.hockeymanager.service.HockeyPlayerService;
+import cz.fi.muni.pa165.hockeymanager.service.TeamService;
+import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
-
+/**
+ * @author Lukas Machalek
+ */
 public class HockeyPlayerFacadeImpl implements HockeyPlayerFacade {
 
-    final static Logger log = LoggerFactory.getLogger(HockeyPlayerFacadeImpl.class);
+    @Autowired
+    private BeanMappingService beanMappingService;
 
     @Inject
     private HockeyPlayerService hockeyPlayerService;
 
+    @Inject
+    private TeamService teamService;
+
     @Override
     public long createHockeyPlayer(HockeyPlayerDto hockeyPlayerDto) {
-        return 0;
+        HockeyPlayer mappedHockeyPlayer = beanMappingService.mapTo(hockeyPlayerDto, HockeyPlayer.class);
+        Team playerTeam = beanMappingService.mapTo(hockeyPlayerDto.getTeam(), Team.class);
+        if(teamService.findById(playerTeam.getId()) != null){
+            teamService.create(playerTeam);
+        }
+        mappedHockeyPlayer.setTeam(playerTeam);
+        hockeyPlayerService.create(mappedHockeyPlayer);
+        //hockeyPlayerService has void as return type for create. Im not sure if this returns the correct id.
+        //It can be reworked to work as the model project, but hockeyPlayerService has to be changed for that.
+        //Tell me in PR and i will rework it if needed.
+        return mappedHockeyPlayer.getId();
     }
 
     @Override
     public void removeHockeyPlayer(long hockeyPlayerId) {
-
+        //This also needs a touch inside hockeyPLayerService i guess.. this doesn't look good.
+        hockeyPlayerService.remove(hockeyPlayerService.findById(hockeyPlayerId));
     }
 
     @Override
     public List<HockeyPlayerDto> getAllHockeyPlayers() {
-        return null;
+        List<HockeyPlayer> hockeyPlayers = hockeyPlayerService.getAllPlayers();
+        List<HockeyPlayerDto> hockeyPlayerDtos = new ArrayList<>();
+        for(HockeyPlayer p : hockeyPlayers){
+            HockeyPlayerDto pDto = beanMappingService.mapTo(p, HockeyPlayerDto.class);
+            hockeyPlayerDtos.add(pDto);
+        }
+        return hockeyPlayerDtos;
     }
 }
