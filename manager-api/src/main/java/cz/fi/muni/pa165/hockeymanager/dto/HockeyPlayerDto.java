f51d86cbe28bb95183b0c94e08934a6fa2388c99
jarkovic <17matus@gmail.com>
2021-04-29 16:44:36 +0200

    hockeyplayer dto impl

--- manager-api/src/main/java/cz/fi/muni/pa165/hockeymanager/dto/HockeyPlayerDto.java
+++ manager-api/src/main/java/cz/fi/muni/pa165/hockeymanager/dto/HockeyPlayerDto.java
@@ -1,4 +1,70 @@
 package cz.fi.muni.pa165.hockeymanager.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+/**
+ * @author Matus Jarkovic 456441
+ */
 public class HockeyPlayerDto {
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String firstName;
+
+    @Getter
+    @Setter
+    private String lastName;
+
+    @Getter
+    @Setter
+    private Integer offensiveStrength;
+
+    @Getter
+    @Setter
+    private Integer deffensiveStrength;
+
+    @Getter
+    @Setter
+    private TeamDto team;
+
+    public HockeyPlayerDto() {}
+
+    public HockeyPlayerDto(String firstName, String lastName, Integer offStrength, Integer deffStrength) {
+        this(firstName, lastName, offStrength, deffStrength, null);
+    }
+
+    public HockeyPlayerDto(String firstName, String lastName, Integer offStrength, Integer deffStrength, TeamDto team) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.offensiveStrength = offStrength;
+        this.deffensiveStrength = deffStrength;
+        this.team = team;
+    }
+
+    public String getFullName() {
+        return this.firstName + this.lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HockeyPlayerDto that = (HockeyPlayerDto) o;
+        return firstName.equals(that.firstName) &&
+                lastName.equals(that.lastName) &&
+                offensiveStrength.equals(that.offensiveStrength) &&
+                deffensiveStrength.equals(that.deffensiveStrength) &&
+                Objects.equals(team, that.team);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, offensiveStrength, deffensiveStrength, team);
+    }
 }
