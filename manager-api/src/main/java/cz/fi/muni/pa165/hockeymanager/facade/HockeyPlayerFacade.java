294154bc010285a583f22048874025db118bc953
Lukas Machalek <xmachal6@fi.muni.cz>
2021-04-29 20:43:25 +0200

    Simple HockeyPlayerFacade

--- manager-api/src/main/java/cz/fi/muni/pa165/hockeymanager/facade/HockeyPlayerFacade.java
+++ manager-api/src/main/java/cz/fi/muni/pa165/hockeymanager/facade/HockeyPlayerFacade.java
@@ -1,4 +1,11 @@
 package cz.fi.muni.pa165.hockeymanager.facade;
 
+import cz.fi.muni.pa165.hockeymanager.dto.HockeyPlayerDto;
+import java.util.List;
+
 public interface HockeyPlayerFacade {
+    long createHockeyPlayer(HockeyPlayerDto hockeyPlayerDto);
+    void removeHockeyPlayer(long hockeyPlayerId);
+    List<HockeyPlayerDto> getAllHockeyPlayers();
+
 }
