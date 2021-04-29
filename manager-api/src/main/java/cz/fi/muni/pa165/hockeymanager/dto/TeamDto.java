7a39a7f4d2b4a532cd3d17e9c725a6e7b650411a
Petr Sopf <trsak1@seznam.cz>
2021-04-29 22:10:19 +0200

    Added Team DTO

--- manager-api/src/main/java/cz/fi/muni/pa165/hockeymanager/dto/TeamDto.java
+++ manager-api/src/main/java/cz/fi/muni/pa165/hockeymanager/dto/TeamDto.java
@@ -1,4 +1,52 @@
 package cz.fi.muni.pa165.hockeymanager.dto;
 
+import cz.fi.muni.pa165.hockeymanager.enums.Championship;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author Petr Šopf (506511)
+ */
 public class TeamDto {
+    @Getter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private Championship championship;
+
+    @Getter
+    @Setter
+    private Long points = 0L;
+
+    @Getter
+    @Setter
+    private UserDto manager = null;
+
+    @Getter
+    private Set<HockeyPlayerDto> hockeyPlayers = new HashSet<>();
+
+    @Getter
+    private Set<MatchDto> matches = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TeamDto team = (TeamDto) o;
+        return name.equals(team.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
