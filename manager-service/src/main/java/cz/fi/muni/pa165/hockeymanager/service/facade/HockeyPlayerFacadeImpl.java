829d870d779aa488607dfb444db8df068fb80810
Lukas Machalek <xmachal6@fi.muni.cz>
2021-04-29 20:47:10 +0200

    Simple HockeyPlayerFacadeImpl

--- manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/facade/HockeyPlayerFacadeImpl.java
+++ manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/facade/HockeyPlayerFacadeImpl.java
@@ -1,4 +1,33 @@
 package cz.fi.muni.pa165.hockeymanager.service.facade;
 
-public class HockeyPlayerFacadeImpl {
+import cz.fi.muni.pa165.hockeymanager.dto.HockeyPlayerDto;
+import cz.fi.muni.pa165.hockeymanager.facade.HockeyPlayerFacade;
+import cz.fi.muni.pa165.hockeymanager.service.HockeyPlayerService;
+import java.util.List;
+import javax.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class HockeyPlayerFacadeImpl implements HockeyPlayerFacade {
+
+    final static Logger log = LoggerFactory.getLogger(HockeyPlayerFacadeImpl.class);
+
+    @Inject
+    private HockeyPlayerService hockeyPlayerService;
+
+    @Override
+    public long createHockeyPlayer(HockeyPlayerDto hockeyPlayerDto) {
+        return 0;
+    }
+
+    @Override
+    public void removeHockeyPlayer(long hockeyPlayerId) {
+
+    }
+
+    @Override
+    public List<HockeyPlayerDto> getAllHockeyPlayers() {
+        return null;
+    }
 }
