31938718ad7090983781e03c2e47e45b67ec23f2
xkosorin <kristian@kosorin.eu>
2021-04-29 09:59:31 +0200

    Added UserService and UserServiceImpl

--- manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/UserService.java
+++ manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/UserService.java
@@ -1,4 +1,15 @@
 package cz.fi.muni.pa165.hockeymanager.service;
 
+import cz.fi.muni.pa165.hockeymanager.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
+    void create(User user);
+    void update(User user);
+    void remove(User user);
+
+    List<User> findAll();
+    User findById(Long id);
 }
--- manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/UserServiceImpl.java
+++ manager-service/src/main/java/cz/fi/muni/pa165/hockeymanager/service/UserServiceImpl.java
@@ -1,4 +1,38 @@
 package cz.fi.muni.pa165.hockeymanager.service;
 
-public class UserServiceImpl {
+import cz.fi.muni.pa165.hockeymanager.dao.UserDao;
+import cz.fi.muni.pa165.hockeymanager.entity.User;
+import org.dozer.inject.Inject;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    @Inject
+    private UserDao userDao;
+
+    @Override
+    public void create(User user) {
+        userDao.create(user);
+    }
+
+    @Override
+    public void update(User user) {
+        userDao.update(user);
+    }
+
+    @Override
+    public void remove(User user) {
+        userDao.remove(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userDao.findById(id);
+    }
 }
