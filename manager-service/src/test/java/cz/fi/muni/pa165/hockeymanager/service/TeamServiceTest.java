369c7db96ca26c837ceae8917c7b69e2f2aefdd5
Lukas Machalek <xmachal6@fi.muni.cz>
2021-05-02 20:02:28 +0200

    Basic TeamServiceTest preparations impl

--- manager-service/src/test/java/cz/fi/muni/pa165/hockeymanager/service/TeamServiceTest.java
+++ manager-service/src/test/java/cz/fi/muni/pa165/hockeymanager/service/TeamServiceTest.java
@@ -1,4 +1,53 @@
 package cz.fi.muni.pa165.hockeymanager.service;
 
-public class TeamServiceTest {
+import cz.fi.muni.pa165.hockeymanager.dao.TeamDao;
+import cz.fi.muni.pa165.hockeymanager.entity.HockeyPlayer;
+import cz.fi.muni.pa165.hockeymanager.entity.Team;
+import cz.fi.muni.pa165.hockeymanager.enums.Championship;
+import cz.fi.muni.pa165.hockeymanager.exceptions.ManagerServiceException;
+import cz.fi.muni.pa165.hockeymanager.service.config.ServiceConfiguration;
+import org.hibernate.service.spi.ServiceException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+
+@ContextConfiguration(classes = ServiceConfiguration.class)
+public class TeamServiceTest extends AbstractTestNGSpringContextTests{
+
+    @Mock
+    private TeamDao teamDao;
+
+    @Autowired
+    @InjectMocks
+    private TeamService teamService;
+
+    private Team team1;
+    private Team team2;
+
+    @BeforeClass
+    public void setup() throws ServiceException {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @BeforeMethod
+    public void resetMock(){
+        reset(teamDao);
+    }
+
+    @BeforeMethod
+    public void prepareTeams(){
+        team1 = new Team("team1", Championship.SHL);
+        team2 = new Team("team2", Championship.DEL);
+    }
 }
