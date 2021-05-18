bacf35a2aba0161370857862730f4cd236a3190b
jarkovic <17matus@gmail.com>
2021-05-18 13:00:55 +0200

    added closing mocks in user service

--- manager-service/src/test/java/cz/fi/muni/pa165/hockeymanager/service/UserServiceTest.java
+++ manager-service/src/test/java/cz/fi/muni/pa165/hockeymanager/service/UserServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -39,9 +40,16 @@ public class UserServiceTest extends AbstractTestNGSpringContextTests {
 
     final private List<User> userList = List.of(user1, user2);
 
+    private AutoCloseable mocks;
+
     @BeforeMethod
     public void setup() {
-        MockitoAnnotations.openMocks(this);
+        mocks = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterMethod
+    public void closeMocks() throws Exception {
+        mocks.close();
     }
 
     @Test
