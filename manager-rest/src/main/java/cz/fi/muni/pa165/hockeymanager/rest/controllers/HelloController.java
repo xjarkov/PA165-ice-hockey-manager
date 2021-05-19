47fe726dfef3f75ab020132e6c1e22a2f3b7fbbf
xkosorin <kristian@kosorin.eu>
2021-05-19 21:47:45 +0200

    REST controller update

--- manager-rest/src/main/java/cz/fi/muni/pa165/hockeymanager/rest/controllers/HelloController.java
+++ manager-rest/src/main/java/cz/fi/muni/pa165/hockeymanager/rest/controllers/HelloController.java
@@ -1,13 +1,13 @@
 package cz.fi.muni.pa165.hockeymanager.rest.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping( "/hello" )
 public class HelloController {
-
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String hello() {
         return "Hello there!";
     }
