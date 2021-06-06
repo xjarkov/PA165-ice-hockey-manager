9cb75862c2bb64466f16186900c98cb1e806c622
Petr Sopf <trsak1@seznam.cz>
2021-06-06 14:22:31 +0200

    Fix all rest endpoints

--- manager-rest/src/main/java/cz/fi/muni/pa165/hockeymanager/rest/controllers/HockeyPlayerController.java
+++ manager-rest/src/main/java/cz/fi/muni/pa165/hockeymanager/rest/controllers/HockeyPlayerController.java
@@ -1,8 +1,6 @@
 package cz.fi.muni.pa165.hockeymanager.rest.controllers;
 
 import cz.fi.muni.pa165.hockeymanager.dto.HockeyPlayerDto;
-import cz.fi.muni.pa165.hockeymanager.dto.MatchDto;
-import cz.fi.muni.pa165.hockeymanager.dto.UserDto;
 import cz.fi.muni.pa165.hockeymanager.facade.HockeyPlayerFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HockeyPlayerController {
@@ -20,14 +21,39 @@ public class HockeyPlayerController {
     private HockeyPlayerFacade hockeyPlayerFacade;
 
     @GetMapping(value = "/hockey_players", produces = MediaType.APPLICATION_JSON_VALUE)
-    public final List<HockeyPlayerDto> getAll() {
+    public final List<Map<String, Object>> getAll() {
         logger.info("REST HockeyPlayers findAll()");
-        return hockeyPlayerFacade.findAll();
+        List<HockeyPlayerDto> players = hockeyPlayerFacade.findAll();
+        List<Map<String, Object>> playersMapped = new ArrayList<>();
+
+        for (HockeyPlayerDto player : players) {
+            playersMapped.add(mapPlayerData(player));
+        }
+
+        return playersMapped;
     }
 
     @GetMapping(value = "/hockey_player/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public final HockeyPlayerDto getPlayer(@PathVariable("id") Long id) {
+    public final Map<String, Object> getPlayer(@PathVariable("id") Long id) {
         logger.info("REST HockeyPlayer findById({})", id);
-        return hockeyPlayerFacade.findById(id);
+        return mapPlayerData(hockeyPlayerFacade.findById(id));
+    }
+
+    private Map<String, Object> mapPlayerData(HockeyPlayerDto hockeyPlayerDto) {
+        Map<String, Object> playerData = new LinkedHashMap<>();
+
+        playerData.put("id", hockeyPlayerDto.getId());
+        playerData.put("firstName", hockeyPlayerDto.getFirstName());
+        playerData.put("lastName", hockeyPlayerDto.getLastName());
+        playerData.put("defensiveStrength", hockeyPlayerDto.getDefensiveStrength());
+        playerData.put("offensiveStrength", hockeyPlayerDto.getOffensiveStrength());
+
+        if (hockeyPlayerDto.getTeam() != null) {
+            playerData.put("team", hockeyPlayerDto.getTeam().getId());
+        } else {
+            playerData.put("team", null);
+        }
+
+        return playerData;
     }
 }
--- manager-rest/src/main/java/cz/fi/muni/pa165/hockeymanager/rest/controllers/MatchController.java
+++ manager-rest/src/main/java/cz/fi/muni/pa165/hockeymanager/rest/controllers/MatchController.java
@@ -1,6 +1,5 @@
 package cz.fi.muni.pa165.hockeymanager.rest.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import cz.fi.muni.pa165.hockeymanager.dto.MatchDto;
 import cz.fi.muni.pa165.hockeymanager.dto.TeamDto;
 import cz.fi.muni.pa165.hockeymanager.facade.MatchFacade;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,21 +25,30 @@ public class MatchController {
     private MatchFacade matchFacade;
 
     @GetMapping(value = "/matches", produces = MediaType.APPLICATION_JSON_VALUE)
-    public final List<MatchDto> getAll() {
+    public final List<Map<String, Object>> getAll() {
         logger.info("REST Match findAllMatches()");
-        return matchFacade.findAllMatches();
+        List<MatchDto> matches = matchFacade.findAllMatches();
+        List<Map<String, Object>> matchesMapped = new ArrayList<>();
+
+        for (MatchDto match : matches) {
+            matchesMapped.add(mapMatchData(match));
+        }
+
+        return matchesMapped;
     }
 
     @GetMapping(value = "/match/nearest", produces = MediaType.APPLICATION_JSON_VALUE)
-    public final MatchDto getNearest() {
+    public final Map<String, Object> getNearest() {
         logger.info("REST Match findNearestMatch()");
-        return matchFacade.findNearestMatch();
+        MatchDto match = matchFacade.findNearestMatch();
+        return mapMatchData(match);
     }
 
     @GetMapping(value = "/match/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public final MatchDto getMatch(@PathVariable("id") Long id) {
+    public final Map<String, Object> getMatch(@PathVariable("id") Long id) {
         logger.info("REST Match findMatchById({})", id);
-        return matchFacade.findMatchById(id);
+        MatchDto match = matchFacade.findMatchById(id);
+        return mapMatchData(match);
     }
 
     @GetMapping(value = "/match/{id}/score", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -49,7 +58,7 @@ public class MatchController {
     }
 
     @GetMapping(value = "/match/{id}/winner", produces = MediaType.APPLICATION_JSON_VALUE)
-    public final Map<String, Object> getMatchWinner(@PathVariable("id") Long id) throws JsonProcessingException {
+    public final Map<String, Object> getMatchWinner(@PathVariable("id") Long id) {
         logger.info("REST Match getMatchWinner({})", id);
 
         TeamDto teamWinner = matchFacade.getWinningTeam(id);
@@ -59,5 +68,17 @@ public class MatchController {
         winningTeam.put("name", teamWinner.getName());
 
         return winningTeam;
+    }
+
+    private Map<String, Object> mapMatchData(MatchDto match) {
+        Map<String, Object> matchData = new LinkedHashMap<>();
+
+        matchData.put("id", match.getId());
+        matchData.put("datetime", match.getDateTimeDto());
+        matchData.put("visitingTeamScore", match.getVisitingTeamScore());
+        matchData.put("visitingHomeScore", match.getHomeTeamScore());
+        matchData.put("homeTeamId", match.getHomeTeam().getId());
+        matchData.put("visitingTeamId", match.getVisitingTeam().getId());
+        return matchData;
     }
 }
