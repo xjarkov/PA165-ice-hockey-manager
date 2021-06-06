34f0c68d1bf60259c150e73bcc726dccbb183879
jarkovic <17matus@gmail.com>
2021-06-06 15:44:56 +0200

    completed management, added constraints

--- manager-api/src/main/java/cz/fi/muni/pa165/hockeymanager/dto/HockeyPlayerCreateDto.java
+++ manager-api/src/main/java/cz/fi/muni/pa165/hockeymanager/dto/HockeyPlayerCreateDto.java
@@ -2,21 +2,30 @@ package cz.fi.muni.pa165.hockeymanager.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Data
 public class HockeyPlayerCreateDto {
     @NotNull
+    @Size(min = 2, max = 20)
     private String firstName;
 
     @NotNull
+    @Size(min = 2, max = 20)
     private String lastName;
 
     @NotNull
+    @Min(0)
+    @Max(100)
     private Integer offensiveStrength;
 
     @NotNull
+    @Min(0)
+    @Max(100)
     private Integer defensiveStrength;
 
     private TeamDto team = null;
--- manager-api/src/main/java/cz/fi/muni/pa165/hockeymanager/dto/HockeyPlayerDto.java
+++ manager-api/src/main/java/cz/fi/muni/pa165/hockeymanager/dto/HockeyPlayerDto.java
@@ -1,7 +1,11 @@
 package cz.fi.muni.pa165.hockeymanager.dto;
 
 import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 /**
@@ -12,15 +16,21 @@ public class HockeyPlayerDto {
     private Long id;
 
     @NotNull
+    @Size(min = 2, max = 20)
     private String firstName;
 
     @NotNull
+    @Size(min = 2, max = 20)
     private String lastName;
 
     @NotNull
+    @Min(0)
+    @Max(100)
     private Integer offensiveStrength;
 
     @NotNull
+    @Min(0)
+    @Max(100)
     private Integer defensiveStrength;
 
     private TeamDto team = null;
